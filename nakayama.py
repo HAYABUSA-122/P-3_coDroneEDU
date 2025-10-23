bd40fbbf1e00918c37965028f43d246c8fac502c
RenKuon <kd1380248@st.kobedenshi.ac.jp>
2025-10-23 15:44:10 +0900

    関数を使用し、ドローンの基本動作を実装

--- nakayama.py
+++ nakayama.py
@@ -7,8 +7,21 @@ drone.pair()
 
 # 離陸する
 drone.takeoff()
+
+
 # 3秒ホバリング(空中で止まる)
 drone.hover(3)
+
+drone.move_forward(distance=100, units="cm", speed=1)
+drone.move_backward(distance=50, units="cm", speed=1)
+drone.move_left(distance=50, units="cm", speed=1)
+drone.move_right(distance=50, units="cm", speed=1)
+
+drone.turn_left(360)
+drone.turn_right(360)
+
+drone.hover(3)
+
 # 着陸する
 drone.land()
 
