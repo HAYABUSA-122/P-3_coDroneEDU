eca02778c3ae37cfdf7b4a473ea658f7fd83301a
RenKuon <kd1380248@st.kobedenshi.ac.jp>
2025-10-23 11:25:24 +0900

    主に変更する箇所を強調表示

--- nakayama.py
+++ nakayama.py
@@ -3,12 +3,16 @@ from codrone_edu.drone import *
 drone=Drone()
 # ドローンとPCを接続する
 drone.pair()
+
+
 # 離陸する
 drone.takeoff()
 # 3秒ホバリング(空中で止まる)
 drone.hover(3)
 # 着陸する
 drone.land()
+
+
 # 接続を解除する
 drone.close()
 
