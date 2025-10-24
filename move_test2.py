e407dc41f4e32e71d1c7458e5a680ac7dfa966c4
RenKuon <kd1380248@st.kobedenshi.ac.jp>
2025-10-24 12:25:41 +0900

    コメント追加

--- move_test2.py
+++ move_test2.py
@@ -1,31 +1,32 @@
 def move_test(drone):
 
     # 3秒ホバリング(空中で止まる)
-    drone.hover(3)
+    drone.hover(3)                                              # ホバリング 3s
 
-    drone.move_distance(0, 0, 0.7, 1)
+    # 以降処理毎に ホバリング 1s を実行
+    drone.move_distance(0, 0, 0.7, 1)                           # 下降：0.7m
     drone.hover(1)
-    drone.move_forward(distance=100, units="cm", speed=1)
+    drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
     drone.hover(1)
-    drone.move_distance(0, 0, 0.5, 1)
+    drone.move_distance(0, 0, 0.5, 1)                           # 上昇：0.5m
     drone.hover(1)
-    drone.turn_right(180)
+    drone.turn_right(180)                                       # 右旋回 180°
     drone.hover(1)
-    drone.move_forward(distance=100, units="cm", speed=1)
+    drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
     drone.hover(1)
-    drone.move_distance(0, 0, -0.5, 1)
+    drone.move_distance(0, 0, -0.5, 1)                          # 下降：0.5m
     drone.hover(1)
-    drone.turn_right(180)
+    drone.turn_right(180)                                       # 右旋回 180°
     drone.hover(1)
-    drone.move_forward(distance=100, units="cm", speed=1)
+    drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
     drone.hover(1)
-    drone.move_distance(0, 0, 0.5, 1)
+    drone.move_distance(0, 0, 0.5, 1)                           # 上昇：0.5m
     drone.hover(1)
-    drone.turn_right(180)
+    drone.turn_right(180)                                       # 右旋回 180°
     drone.hover(1)
-    drone.move_forward(distance=100, units="cm", speed=1)
+    drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
 
-    drone.hover(3)
+    drone.hover(3)                                              # ホバリング 3s
 
 
     # drone.move_forward(distance=100, units="cm", speed=1)
