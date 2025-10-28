40f48fd91e178f1a6cb6f73ab088bb47c39feab0
RenKuon <kd1380248@st.kobedenshi.ac.jp>
2025-10-28 16:09:48 +0900

    仮コミット

--- move_test2.py
+++ move_test2.py
@@ -27,17 +27,3 @@ def move_test(drone):
     drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
 
     drone.hover(3)                                              # ホバリング 3s
-
-
-    # drone.move_forward(distance=100, units="cm", speed=1)
-    # drone.hover(1)
-    # drone.move_backward(distance=50, units="cm", speed=1)
-    # drone.hover(1)
-    # drone.move_left(distance=50, units="cm", speed=1)
-    # drone.hover(1)
-    # drone.move_right(distance=50, units="cm", speed=1)
-    # drone.hover(1)
-    # drone.turn_left(360)
-    # drone.hover(1)
-    # drone.turn_right(360)
-    # drone.hover(1)
