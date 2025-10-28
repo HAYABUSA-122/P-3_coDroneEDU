fc9f215bd7e0b609582f797553695a237a2f3ae0
RenKuon <kd1380248@st.kobedenshi.ac.jp>
2025-10-28 16:19:05 +0900

    前進をセンサーからジャイロに変更

--- move_test2.py
+++ move_test2.py
@@ -1,3 +1,6 @@
+from main import drone
+
+
 def move_test(drone):
 
     # 3秒ホバリング(空中で止まる)
@@ -6,24 +9,66 @@ def move_test(drone):
     # 以降処理毎に ホバリング(1s) を実行
     drone.move_distance(0, 0, 0.7, 1)                           # 下降：0.7m, speed 1m/s
     drone.hover(1)
-    drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
+
+    drone.set_pitch(30)
+    drone.set_roll(0)
+    drone.move(2)       # 前進：100cm, speed 1m/s
     drone.hover(1)
+
     drone.move_distance(0, 0, 0.5, 1)                           # 上昇：0.5m, speed 1m/s
     drone.hover(1)
+
     drone.turn_right(180)                                       # 右旋回 180°
     drone.hover(1)
-    drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
+
+    drone.set_pitch(30)                                         # 前進：100cm, speed 1m/s
+    drone.set_roll(0)
+    drone.move(2)
     drone.hover(1)
+
     drone.move_distance(0, 0, -0.5, 1)                          # 下降：0.5m, speed 1m/s
     drone.hover(1)
+
     drone.turn_right(180)                                       # 右旋回 180°
     drone.hover(1)
-    drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
+
+    drone.set_pitch(30)                                         # 前進：100cm, speed 1m/s
+    drone.set_roll(0)
+    drone.move(2)
     drone.hover(1)
+
     drone.move_distance(0, 0, 0.5, 1)                           # 上昇：0.5m, speed 1m/s
     drone.hover(1)
+
     drone.turn_right(180)                                       # 右旋回 180°
     drone.hover(1)
-    drone.move_forward(distance=100, units="cm", speed=1)       # 前進：100cm, speed 1m/s
 
-    drone.hover(3)                                              # ホバリング 3s
+    drone.set_pitch(30)                                         # 前進：100cm, speed 1m/s
+    drone.set_roll(0)
+    drone.move(2)
+
+    drone.hover(3)
+
+
+
+    # メモ
+
+    # # 前に移動
+    # drone.set_pitch(30)
+    # drone.set_roll(0)
+    # drone.move(2)
+
+    # # 右に移動
+    # drone.set_pitch(0)
+    # drone.set_roll(30)
+    # drone.move(2)
+
+    # # 後ろに移動
+    # drone.set_pitch(-30)
+    # drone.set_roll(0)
+    # drone.move(2)
+
+    # # 左に移動
+    # drone.set_pitch(0)
+    # drone.set_roll(-30)
+    # drone.move(2)
