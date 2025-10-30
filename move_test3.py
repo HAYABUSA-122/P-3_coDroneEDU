4b6fd9e0b7bda4668e16da1103fc5f01c921165e
kikuchi <natumetarou@gmail.com>
2025-10-30 16:27:50 +0900

    きくち♡

--- move_test3.py
+++ move_test3.py
@@ -1,17 +1,25 @@
 from codrone_edu.drone import Drone
+import time
 
-PIPE_SHORT = 1  #0.2 (200ミリ)
-PIPE_RONG = 1  #1.524 (1524ミリ)
+PIPE_LONG = 1.524
+PIPE_SHORT = 0.2
 
+DROP_POLE = -0.275
 
+#　処理
 def move_test(drone: Drone):
 
+
     drone.hover(1)
-    drone.move_distance(PIPE_RONG, 0, 0,1)   # x = 3.5
-    drone.hover(1)
-    drone.move_distance(0, 0, -0.3, 0.5)
-    drone.hover(1)
-    drone.move_distance(PIPE_RONG, 0, 0, 0.2) # x = 3.048
-    drone.hover(1)
+    time.sleep(1)
+    print("現在の高度：", drone.get_height())
+    #drone.move_distance(0, 0, DROP_POLE,1)  # xが奥行き,yが横移動(正の数が左,負の数が右),ｚが高さ,veloctiyが速度
+    #drone.hover(1)
+
+    #drone.move_distance(0, 0, 0.275, 1)
+    #drone.hover(1)
+    #print("現在の高度",drone.get_height(2))
+    # drone.move_distance(0.5, 0.5, 0,1)
+
 
 
